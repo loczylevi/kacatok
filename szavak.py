d9c836dab42790be2d83d0108791297d4c5f475c
loczylevi <71691777+loczylevi@users.noreply.github.com>
2022-02-03 17:22:22 +0100

    Create szavak.py

---
+++ szavak.py
@@ -0,0 +1,1 @@
+szavak = ["hógolyó", "oroszlán", "istálló", "orvosság", "sintér", "hazugság", "közönséges", "apró", "keskeny","testvér", "vélemény", "tanács","vitorla","manapság", "tábortüz", "csöndes", "varázsló", "gyüjt", "kémkedik", "földnyelv", "villám", "zöldfülű","kellemetlen", "elpusztít", "földöntúli", "parancs", "légy", "veterán", "martalóc","szabotázs"]
